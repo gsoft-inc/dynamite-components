bdca91fe9ac198bbb27120e15c587d28173f7c86
Franck Cornu <franck.cornu@gsoft-group.com>
2014-09-25 14:42:52 -0400

    - Removing unused usings - Add DisplayTemplateInfos configuration

---
+++ Source/GSoft.Dynamite.Publishing.Contracts/Constants/BasePublishingDisplayTemplateInfos.cs
@@ -0,0 +1,22 @@
+﻿using GSoft.Dynamite.Definitions;
+
+namespace GSoft.Dynamite.Publishing.Contracts.Constants
+{
+    public class BasePublishingDisplayTemplateInfos
+    {
+        public DisplayTemplateInfo ItemSingleNewsItemHeader()
+        {
+            return new DisplayTemplateInfo("Item_SingleNewsItemHeader", DisplayTemplateCategory.Search);
+        }
+
+        public DisplayTemplateInfo ItemSingleNewsItemContent()
+        {
+            return new DisplayTemplateInfo("Item_SingleNewsItemContent", DisplayTemplateCategory.Search);
+        }
+
+        public DisplayTemplateInfo ItemSingleContentItem()
+        {
+            return new DisplayTemplateInfo("tem_SingleContentItem", DisplayTemplateCategory.Search);
+        }
+    }
+}
