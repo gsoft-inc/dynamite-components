537113edcb1cbdbe5db3aa14d3ee14f3dc762a1c
Philippe Lavoie <phil@philippelavoie.com>
2014-09-29 10:59:41 -0400

    [Script Nuspec] Debug the script to see if it worked.

--- Source/GSoft.Dynamite.CrossSitePublishingCMS/Install.ps1
+++ Source/GSoft.Dynamite.CrossSitePublishingCMS/Install.ps1
@@ -2,6 +2,13 @@
 
 $hostname = [System.Net.Dns]::GetHostName()
 
+New-Item .\log.txt -type file
+Add-Content .\log.txt $hostname
+Add-Content .\log.txt $installPath
+Add-Content .\log.txt $toolsPath
+Add-Content .\log.txt $package
+Add-Content .\log.txt $project
+
 if (Test-Path $installPath\Tokens.HOSTNAME.ps1 -and Test-Path $installPath\Tokens.HOSTNAME.ps1) {
     Rename-Item $installPath\Tokens.HOSTNAME.ps1 $installPath\Tokens.$hostname.ps1
 }
