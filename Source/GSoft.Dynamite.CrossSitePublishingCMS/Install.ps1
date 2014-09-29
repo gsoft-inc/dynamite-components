81f0659baadc9a8bb8a943258fa2435598620243
Philippe Lavoie <phil@philippelavoie.com>
2014-09-29 10:15:18 -0400

    [Nuspec run script] Actually read the doc and put the proper convention name

---
+++ Source/GSoft.Dynamite.CrossSitePublishingCMS/Install.ps1
@@ -0,0 +1,7 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$hostname = [System.Net.Dns]::GetHostName()
+
+if (Test-Path $installPath\Tokens.HOSTNAME.ps1 -and Test-Path $installPath\Tokens.HOSTNAME.ps1) {
+    Rename-Item $installPath\Tokens.HOSTNAME.ps1 $installPath\Tokens.$hostname.ps1
+}
--- Source/GSoft.Dynamite.CrossSitePublishingCMS/Setup-Token.ps1
+++
@@ -1,5 +0,0 @@
-﻿$hostname = [System.Net.Dns]::GetHostName()
-
-if (Test-Path ./Tokens.HOSTNAME.ps1 -and Test-Path ./Tokens.HOSTNAME.ps1) {
-    Rename-Item Tokens.HOSTNAME.ps1 Tokens.$hostname.ps1
-}
