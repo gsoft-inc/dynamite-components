c5eda668c5098f21132d5146b3c6f888d0999274
Philippe Lavoie <phil@philippelavoie.com>
2014-09-29 09:44:16 -0400

    [Token Script Setup Nuget] First test for the nuget setup script execution.

---
+++ Source/GSoft.Dynamite.CrossSitePublishingCMS/Setup-Token.ps1
@@ -0,0 +1,5 @@
+﻿$hostname = [System.Net.Dns]::GetHostName()
+
+if (Test-Path ./Tokens.HOSTNAME.ps1 -and Test-Path ./Tokens.HOSTNAME.ps1) {
+    Rename-Item Tokens.HOSTNAME.ps1 Tokens.$hostname.ps1
+}
