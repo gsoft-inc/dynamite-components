155673b607f1e0210321a384702980fcb5663823
Franck Cornu <franck.cornu@gsoft-group.com>
2014-10-01 11:55:01 -0400

    - Update Nuget Dynamite Package - Including multilingual webs creation

--- Source/GSoft.Dynamite.CrossSitePublishingCMS/Modules/Publishing/PUB_01/Setup-Webs.ps1
+++ Source/GSoft.Dynamite.CrossSitePublishingCMS/Modules/Publishing/PUB_01/Setup-Webs.ps1
@@ -20,5 +20,30 @@ if(![string]::IsNullOrEmpty($CustomConfigurationFile))
 
 [xml]$webXml = Get-Content $ConfigurationFilePath
 
+
+# Check Multilingual settings
+$IsMultilingual = [System.Convert]::ToBoolean("False")
+
+$ParentWebUrl = "http://authoring.dynamite.com"
+
+if($IsMultilingual)
+{
+	# Create webs under the source label root site
+	$ParentWebUrl += "/" + "en"
+}
+
 # Create the new SharePoint Web structure
-New-DSPWebXml -Webs $webXml.Webs -ParentUrl "http://authoring.dynamite.com" -UseParentTopNav -Overwrite
+New-DSPWebXml -Webs $webXml.Webs -ParentUrl $ParentWebUrl -UseParentTopNav -Overwrite | ForEach-Object{
+	if($IsMultilingual)
+	{
+		$Web = $_
+		$DSP_TargetLabels | ForEach-Object {
+
+			# Sync webs for all taget labels
+			Sync-DSPWeb -SourceWeb $Web -LabelToSync $_
+		}
+
+		$webApplication = Get-SPWebApplication "http://franck-vm2013/"
+		Wait-SPTimerJob -Name "VariationsSpawnSites" -WebApplication $webApplication
+	}
+}
