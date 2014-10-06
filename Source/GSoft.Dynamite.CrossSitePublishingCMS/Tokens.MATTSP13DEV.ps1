c8de708c707ac48670859666d8a1a3e0f126e740
Matthieu Fenger <fengermatthieu@live.com>
2014-10-06 11:57:40 -0400

    [Token] Added token to dev environnement

---
+++ Source/GSoft.Dynamite.CrossSitePublishingCMS/Tokens.MATTSP13DEV.ps1
@@ -0,0 +1,46 @@
+﻿# -----------------------------------------
+# Deployment Mode
+# -----------------------------------------
+
+$DSP_IsDistributedEnvironment = "$false"
+
+# ------------------------------------------
+# Common Tokens
+# ------------------------------------------
+. ./Tokens/Tokens.Common.ps1
+
+# ******************************************
+# Deployment Configuration 
+# ******************************************
+
+$DSP_NugetSolutionsScanRootPath = "C:\dev\Dynamite-Components\Libraries"
+$DSP_CustomSolutionsScanRootPath = "C:\dev\Dynamite-Components\Source"
+
+$DSP_CUSTOM_PortalSetupSolutionsConfigurationFile = ".\Custom\Custom-Solutions.xml"
+
+# ******************************************
+# Application Configuration 
+# ******************************************
+$DSP_PortalWebAppUrl = "http://mattsp13dev/"
+$DSP_PortalPublishingHostNamePath = "http://intranet.dynamite.com"
+$DSP_PortalAuthoringHostNamePath = "http://authoring.dynamite.com"
+
+$DSP_PortalAuthoringRootWebUrl = $DSP_PortalAuthoringHostNamePath 
+
+$DSP_PortalAdmin = "OFFICE\matthieu.fenger"
+$DSP_PortalDatabaseName = "SP2013_Content_Portal"
+$DSP_PortalDefaultLanguage = "1033"
+
+# ******************************************
+# Multilingualism Configuration 
+# ******************************************
+
+$DSP_IsMultilingual = $true
+$DSP_VariationsLabels = @('en','fr')
+$DSP_SourceLabel = "en"
+
+# ******************************************
+# Webs Configuration 
+# ******************************************
+
+$DSP_PortalAuthoringRootWebs = @('rh','com')
