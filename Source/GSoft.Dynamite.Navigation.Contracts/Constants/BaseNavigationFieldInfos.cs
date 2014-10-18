bd3db4166c4682482375fd7e6ff14b42849fd258
Franck Cornu <franck.cornu@gsoft-group.com>
2014-10-18 16:13:31 -0400

    - [Navigation] Correction du nom de fichier pour BaseNavigationFieldInfos

--- Source/GSoft.Dynamite.Navigation.Contracts/Constants/BaseNavigationFieldInfos.cs
+++ Source/GSoft.Dynamite.Navigation.Contracts/Constants/BaseNavigationFieldInfos.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using GSoft.Dynamite.Binding;
-using GSoft.Dynamite.Definitions;
 using GSoft.Dynamite.FieldTypes;
 using GSoft.Dynamite.Publishing.Contracts.Constants;
 
