5967c8f0b76e960ea00b55bdbd0373d730a3fc8b
Philippe Lavoie <phil@philippelavoie.com>
2014-11-03 14:03:28 -0500

    [Life Cycle Module] Added the Life Cycle module to the solution. Add the ContentBySearchSchedule WebPart to the LifeCycle Module

---
+++ Source/GSoft.Dynamite.LifeCycle.SP/RegistrationModules/LifeCycleSPRegistrationModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using GSoft.Dynamite.LifeCycle.Contracts.WebParts;
+using GSoft.Dynamite.LifeCycle.SP.WebParts.ContentBySearchSchedule;
+
+namespace GSoft.Dynamite.LifeCycle.SP.RegistrationModules
+{
+    /// <summary>
+    /// Portal Publishing Registration Module for the dependencies injection engine
+    /// </summary>
+    public class LifeCycleSPRegistrationModule : Module
+    {
+        /// <summary>
+        /// Registers the modules type bindings
+        /// </summary>
+        /// <param name="builder">
+        /// The builder.
+        /// </param>
+        protected override void Load(ContainerBuilder builder)
+        {
+            // WebParts
+            builder.RegisterType<ContentBySearchSchedule>().As<IContentBySearchSchedule>().ExternallyOwned();
+        }
+    }
+}
