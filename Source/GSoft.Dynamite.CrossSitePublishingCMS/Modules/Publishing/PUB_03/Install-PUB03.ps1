33138a28581b6862190f16542791cb9aab195aae
Philippe Lavoie <phil@philippelavoie.com>
2014-11-05 15:33:01 -0500

    [Pub03 Feature] Added a feature for the FilteredProductShowcase and the Pub03 module. Fix the GSoft.Dynamite.Publishing.SP signing with the wrong key.

---
+++ Source/GSoft.Dynamite.CrossSitePublishingCMS/Modules/Publishing/PUB_03/Install-PUB03.ps1
@@ -0,0 +1,42 @@
+﻿# ----------------------------------------
+# PUB 03: VIEW ITEM DETAILS
+# ----------------------------------------
+
+param([string]$LogFolderPath)
+
+# ********** LOG INIT ********** #
+
+$0 = $myInvocation.MyCommand.Definition
+$CommandDirectory = [System.IO.Path]::GetDirectoryName($0)
+
+if ([string]::IsNullOrEmpty($LogFolderPath))
+{
+	$LogFolderPath = $CommandDirectory
+}
+
+$ScriptName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
+
+$LogTime = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
+$LogFile = $LogFolderPath + "\" + $ScriptName +"_Dynamite_"+$LogTime +".log"
+
+# Stat log transcript
+Start-Transcript -Path $LogFile 
+
+# ***************************** #
+
+# Verbose preference
+$VerbosePreference ="Continue"
+
+$UserStory = "PUB_03"
+
+##### STEP 1: CREATE SEARCH RESULT SOURCES
+$values = @{"Step: " = "Deploy Web Parts";"User Story: " = $UserStory}
+New-HeaderDrawing -Values $Values
+
+$Script = $CommandDirectory + '\Setup-WebParts.ps1'
+& $Script -Force
+
+# ********** LOG END ********** #
+# Stop log transcript
+Stop-Transcript
+# ***************************** #
