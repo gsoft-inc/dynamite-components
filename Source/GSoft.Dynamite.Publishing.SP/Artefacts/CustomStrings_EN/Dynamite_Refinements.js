be11d895d827ade510343137221f60c1118d4fe5
Franck Cornu <franck.cornu@gsoft-group.com>
2014-11-12 12:57:09 -0500

    - [PUB_03] Ajout d'un display template pour les éléments de refinements + Provisionnement des custom strings pour la traduction des refinements

---
+++ Source/GSoft.Dynamite.Publishing.SP/Artefacts/CustomStrings_EN/Dynamite_Refinements.js
@@ -0,0 +1,5 @@
+﻿$registerResourceDictionary("en-us", {
+    "sampleCustomStringId": "Sample custom string",
+    "rf_RefinementTitle_ManagedPropertyName": "Sample Refinement Title for ManagedPropertyName",
+    "rf_RefinementTitle_DynamiteNavigationOWSTEXT": "Category",
+});
