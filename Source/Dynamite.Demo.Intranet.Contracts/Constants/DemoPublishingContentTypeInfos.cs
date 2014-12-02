56330d2f45f217910e11e5777c6275854ec9f5aa
Franck Cornu <franck.cornu@gsoft-group.com>
2014-12-02 11:06:09 -0500

    - StyleCop projet Demo

--- Source/Dynamite.Demo.Intranet.Contracts/Constants/DemoPublishingContentTypeInfos.cs
+++ Source/Dynamite.Demo.Intranet.Contracts/Constants/DemoPublishingContentTypeInfos.cs
@@ -1,31 +1,29 @@
 ﻿using System.Collections.Generic;
-using System.Globalization;
 using Dynamite.Demo.Intranet.Contracts.Resources;
 using GSoft.Dynamite.ContentTypes;
 using GSoft.Dynamite.Fields;
 using GSoft.Dynamite.Globalization;
 using GSoft.Dynamite.Publishing.Contracts.Constants;
-using Microsoft.SharePoint;
 
 namespace Dynamite.Demo.Intranet.Contracts.Constants
 {
+    /// <summary>
+    /// Content type definitions for the Dynamite demo module
+    /// </summary>
     public class DemoPublishingContentTypeInfos
     {
-        private readonly IResourceLocator _resourceLocator;
-        private readonly string _resourceFileName = DynamiteDemoResources.Global;
         private readonly DemoPublishingFieldInfos _fieldInfoValues;
         private readonly PublishingContentTypeInfos _baseContentTypeValues;
 
         /// <summary>
         /// The DynamiteDemoContentTypeInfoValues constructor
         /// </summary>
-        /// <param name="resourceLocator">The resource locator instance</param>
         /// <param name="fieldInfoValues">The field info instance</param>
-        public DemoPublishingContentTypeInfos(IResourceLocator resourceLocator, DemoPublishingFieldInfos fieldInfoValues, PublishingContentTypeInfos baseContentTypeValues)
+        /// <param name="baseContentTypeValues">The base content type settings</param>
+        public DemoPublishingContentTypeInfos(DemoPublishingFieldInfos fieldInfoValues, PublishingContentTypeInfos baseContentTypeValues)
         {
-            _resourceLocator = resourceLocator;
-            _fieldInfoValues = fieldInfoValues;
-            _baseContentTypeValues = baseContentTypeValues;
+            this._fieldInfoValues = fieldInfoValues;
+            this._baseContentTypeValues = baseContentTypeValues;
         }
 
         #region Dynamite Item
@@ -37,14 +35,14 @@ namespace Dynamite.Demo.Intranet.Contracts.Constants
         public ContentTypeInfo DynamiteItem()
         {
             return new ContentTypeInfo(
-                    _baseContentTypeValues.NewsItem().ContentTypeId +"01",
+                    this._baseContentTypeValues.NewsItem().ContentTypeId + "01",
                     DynamiteDemoResources.ContentTypeDynamiteItemTitle,
                     DynamiteDemoResources.ContentTypeDynamiteItemDescription,
                     DynamiteDemoResources.ContentTypeGroup)
                 {
                     Fields = new List<IFieldInfo>()
                     {
-                        _fieldInfoValues.DynamiteDemoColumn()
+                        this._fieldInfoValues.DynamiteDemoColumn()
                     }
                 };
         }
--- Source/Dynamite.Demo.Intranet.Core/Configuration/DemoPublishingFieldInfoConfig.cs
+++ Source/Dynamite.Demo.Intranet.Core/Configuration/DemoPublishingFieldInfoConfig.cs
@@ -5,18 +5,28 @@ using GSoft.Dynamite.Publishing.Contracts.Configuration;
 
 namespace Dynamite.Demo.Intranet.Core.Configuration
 {
+    /// <summary>
+    /// Example of an override of fields for a client solution
+    /// </summary>
     public class DemoPublishingFieldInfoConfig : IPublishingFieldInfoConfig
     {
         private readonly DemoPublishingFieldInfos _fieldInfoValues;
         private readonly IPublishingFieldInfoConfig _basePublishingFieldInfoConfig;
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="basePublishingFieldInfoConfig">The base fields configuration from the publishing module</param>
+        /// <param name="fieldInfoValues">The fields settings from the dynamite demo solution</param>
         public DemoPublishingFieldInfoConfig(IPublishingFieldInfoConfig basePublishingFieldInfoConfig, DemoPublishingFieldInfos fieldInfoValues)
         {
            this._fieldInfoValues = fieldInfoValues;
            this._basePublishingFieldInfoConfig = basePublishingFieldInfoConfig;
-
         }
 
+        /// <summary>
+        /// Fields override from the publishing module
+        /// </summary>
         public IList<IFieldInfo> Fields
         {
             get
