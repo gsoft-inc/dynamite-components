0986233b065e7c7a3db74e47133f127d7bb17417
Matthieu Fenger <fengermatthieu@gmail.com>
2014-12-17 10:09:08 -0500

    [NAV_01] Fixed interface implementation + StyleCops

--- Source/GSoft.Dynamite.Navigation.Core/Configuration/NavigationCatalogInfoConfig.cs
+++ Source/GSoft.Dynamite.Navigation.Core/Configuration/NavigationCatalogInfoConfig.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GSoft.Dynamite.Catalogs;
+using GSoft.Dynamite.Navigation.Contracts.Configuration;
 
 namespace GSoft.Dynamite.Navigation.Core.Configuration
 {
     /// <summary>
     /// Configuration for the catalogs settings. Catalogs are only used with Cross Site Publishing based solutions
     /// </summary>
-    public class NavigationCatalogInfoConfig
+    public class NavigationCatalogInfoConfig : INavigationCatalogInfoConfig
     {
+        /// <summary>
+        /// The catalog configuration  the navigation module
+        /// </summary>
+        public IList<CatalogInfo> Catalogs
+        {
+            get 
+            {
+                return null;
+            }
+        }
     }
 }
