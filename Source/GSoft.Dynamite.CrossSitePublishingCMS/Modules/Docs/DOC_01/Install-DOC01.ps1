029a944bc136ffa4ed44b34f0b98c9d49d1218d2
Matthieu Fenger <fengermatthieu@gmail.com>
2015-01-20 10:14:32 -0500

    [DOC_01] Added the Blob Cache Configuration in cross site publishing CMS.

---
+++ Source/GSoft.Dynamite.CrossSitePublishingCMS/Modules/Docs/DOC_01/Install-DOC01.ps1
@@ -0,0 +1,46 @@
+﻿# ----------------------------------------
+# DOC 01: ADD MEDIA CONTENT
+# ----------------------------------------
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+param([string]$LogFolderPath)
+
+# ********** LOG INIT ********** #
+
+$0 = $myInvocation.MyCommand.Definition
+$CommandDirectory = [System.IO.Path]::GetDirectoryName($0)
+
+if ([string]::IsNullOrEmpty($LogFolderPath))
+{
+	$LogFolderPath = $CommandDirectory
+}
+
+
+$ScriptName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
+
+$LogTime = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
+$LogFile = $LogFolderPath + "\" + $ScriptName +"_Dynamite_"+$LogTime +".log"
+
+# Stat log transcript
+Start-Transcript -Path $LogFile
+
+# ***************************** #
+
+$UserStory = "DOC_01"
+
+$0 = $myInvocation.MyCommand.Definition
+$CommandDirectory = [System.IO.Path]::GetDirectoryName($0)
+
+$values = @{"User Story: " = $UserStory}
+New-HeaderDrawing -Values $Values
+
+$values = @{"Step: " = "#1 Setup Blob Cache Configuration"}
+New-HeaderDrawing -Values $Values
+
+$Script = $CommandDirectory + '\Setup-BlobCache.ps1'
+& $Script $LogFolderPath
+
+# ********** LOG END ********** #
+# Stop log transcript
+Stop-Transcript
+# ***************************** #
