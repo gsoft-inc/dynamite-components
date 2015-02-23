fec72219abe28618c82b4c795bb5ac6eac1184af
Matthieu Fenger <fengermatthieu@gmail.com>
2015-02-23 10:21:01 -0500

    [SEARCH] Added a new module, SEARCH. SRCH02 Sets the Google Analytics Tracker ID in the property bags of the publishing site collection and enables the feature GSoft.Dynamite.SP Google Analytics Tracking, that feature adds a control in the additionnal page head. If you're updating Components on an existing solution, ADD THE TOKEN $DSP_GoogleAnalyticsUA = "UA-XXXXXXXX" in the appropriate token.HOSTNAME.ps1. Added the module in the instal.model,ps1 of CrossSitePublishingCMS model.

--- Source/GSoft.Dynamite.Models.CrossSitePublishingCMS/Tokens.HOSTNAME.ps1
+++ Source/GSoft.Dynamite.Models.CrossSitePublishingCMS/Tokens.HOSTNAME.ps1
@@ -24,6 +24,7 @@ $DSP_PortalDefaultLanguage = "1033"
 # ******************************************
 $DSP_SearchServiceApplicationName = "Search Service Application"
 $DSP_SearchContentSourceName = "Local SharePoint sites"
+$DSP_GoogleAnalyticsUA = "UA-XXXXXXXX"
 
 # ******************************************
 # Multilingualism Configuration 
