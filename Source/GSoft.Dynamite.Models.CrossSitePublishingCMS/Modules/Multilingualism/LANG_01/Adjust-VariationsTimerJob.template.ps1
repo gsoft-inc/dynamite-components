732713ced2405c5df38ad88fff8ab79fbdee4347
Alexandre Massé <alexandre.masse@gsoft-group.com>
2015-02-27 10:45:17 -0500

    [LANG_01] Added examples of possible timer schedule values for the variations timer job script.

--- Source/GSoft.Dynamite.Models.CrossSitePublishingCMS/Modules/Multilingualism/LANG_01/Adjust-VariationsTimerJob.template.ps1
+++ Source/GSoft.Dynamite.Models.CrossSitePublishingCMS/Modules/Multilingualism/LANG_01/Adjust-VariationsTimerJob.template.ps1
@@ -1,8 +1,9 @@
-﻿# ------------------------------------------------------------------------------
-# Custom Script
-#
-# This script adjusts the Variations Propagate List Items timer job to 5 minutes
-# ------------------------------------------------------------------------------
+﻿# ------------------------------------------------------------------------------------------------------------
+# Copyright		  : GSoft @2015
+# Model  		    : Cross Site Publishing CMS
+# File          : Adjust-VariationsTimerJob.template.ps1
+# Description	  : This script adjusts the Variations Propagate List Items timer job (default to every 5 minutes)
+# ------------------------------------------------------------------------------------------------------------
 
 $webApp = "[[DSP_PortalWebAppUrl]]"
 $defaultTimerSchedule = "[[DSP_DEFAULT_VariationTimerJobFrequence]]"
