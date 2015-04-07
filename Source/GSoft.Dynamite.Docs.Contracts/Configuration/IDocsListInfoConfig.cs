b6d0d8b6c72bf85130db516a29a541fbb4c271d8
Alexandre Massé <alexandre.masse@gsoft-group.com>
2015-04-07 17:55:52 -0400

    [DOC] Added a feature creating document libraries based on a DocsListInfoConfig and it's associated web URL.

---
+++ Source/GSoft.Dynamite.Docs.Contracts/Configuration/IDocsListInfoConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using GSoft.Dynamite.Lists;
+
+namespace GSoft.Dynamite.Docs.Contracts.Configuration
+{
+    /// <summary>
+    /// Lists configuration for the document management module
+    /// </summary>
+    public interface IDocsListInfoConfig
+    {
+        /// <summary>
+        /// Property that return all the lists to create in the document management module
+        /// </summary>
+        IList<ListInfo> DocumentLibraries { get; }
+    }
+}
