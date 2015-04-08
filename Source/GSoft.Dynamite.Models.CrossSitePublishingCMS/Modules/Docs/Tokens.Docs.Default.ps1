fd99026670142868bedb7fc47b783390ac2ed556
Yohan Belval <yohan.belval@gsoft.com>
2015-04-08 15:42:14 -0400

    [NuGet] Changed dynamite to prerelease. [Deployment] Added missing tokens for image renditions.

--- Source/GSoft.Dynamite.Models.CrossSitePublishingCMS/Modules/Docs/Tokens.Docs.Default.ps1
+++ Source/GSoft.Dynamite.Models.CrossSitePublishingCMS/Modules/Docs/Tokens.Docs.Default.ps1
@@ -4,3 +4,4 @@
 
 $DSP_CrossSitePublishingCMS_DOC_Fields = "2ed19e36-a8c2-44e1-a155-cf93186f2c78"
 $DSP_CrossSitePublishingCMS_DOC_ContentTypes = "677b2547-92bc-4344-9e13-487cfddd86b1"
+$DSP_CrossSitePublishingCMS_DOC_ImageRenditions = "30eefbff-d4a4-44f4-81a1-74bcdf9b887b"
