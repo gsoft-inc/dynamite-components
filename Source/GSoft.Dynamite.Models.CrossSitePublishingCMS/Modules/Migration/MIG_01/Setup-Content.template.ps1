fd47f1e14629db09ec643913c92240d24960df58
Yohan Belval <yohan.belval@gsoft.com>
2015-04-16 18:15:12 -0400

    [Migration] Changed the Sharegate migration scripts to use the new Import-DSPData cmdlet.  *First draft*

--- Source/GSoft.Dynamite.Models.CrossSitePublishingCMS/Modules/Migration/MIG_01/Setup-Content.template.ps1
+++ Source/GSoft.Dynamite.Models.CrossSitePublishingCMS/Modules/Migration/MIG_01/Setup-Content.template.ps1
@@ -1,13 +1,11 @@
 ﻿# -----------------------------------------------------------------------
-# Copyright		: GSoft @2014
+# Copyright		: GSoft @2015
 # Model  		: Cross Site Publishing CMS
 # File          : Setup-Content.ps1.template
-# Description	: Import Content into catalogs
+# Description	: Import Content into SharePoint
 # -----------------------------------------------------------------------
 
 param([string]$LogFolderPath)
-
-# Configure Reports files
 
 $0 = $myInvocation.MyCommand.Definition
 $CommandDirectory = [System.IO.Path]::GetDirectoryName($0)
@@ -17,213 +15,130 @@ if ([string]::IsNullOrEmpty($LogFolderPath))
 	$LogFolderPath = $CommandDirectory
 }
 
-# -----------------------------------------------------------------------
-# TOKENS SETTINGS
-# -----------------------------------------------------------------------
-
-# Check Multilingual settings
-$IsMultilingual = [System.Convert]::ToBoolean("[[DSP_IsMultilingual]]")
-$SourceLabel ="[[DSP_SourceLabel]]"
-
-### IMAGES CONFIGURATION
-
-$DefaultImagesFolder = "[[DSP_DEFAULT_ImagesSourceFolder]]"
-$CustomImagesFolder = "[[DSP_CUSTOM_ImagesSourceFolder]]"
-
-### EXCEL IMPORT FILES CONFIGURATION
-
-$DefaultExcelFilesFolder = "[[DSP_DEFAULT_ExcelFilesSourceFolder]]"
-$CustomExcelFilesFolder = "[[DSP_CUSTOM_ExcelFilesSourceFolder]]"
-
-### SHAREGATE TEMPLATE CONFIGURATION
-
-$DefaultPropertyTemplateFile = "[[DSP_DEFAULT_PropertyTemplateFile]]"
-$CustomPropertyTemplateFile = "[[DSP_CUSTOM_PropertyTemplateFile]]"
-
-$DefaultPropertyTemplateName = "[[DSP_DEFAULT_PropertyTemplateName]]"
-$CustomPropertyTemplateName = "[[DSP_CUSTOM_PropertyTemplateName]]"
-
-### CATALOGS MAPPINGS
-
-$CustomCatalogsTitles = "[[DSP_CUSTOM_CatalogTitles]]"
-
-$ImagesConfigurationFolder = $CommandDirectory  + $DefaultImagesFolder
-$DefaultExcelConfigurationFolder = $CommandDirectory + $DefaultExcelFilesFolder
-$DefaultPropertyTemplateFile = $CommandDirectory + $DefaultPropertyTemplateFile
-
-if(![string]::IsNullOrEmpty($CustomImagesFolder))
-{
-	$ImagesConfigurationFolder = $CommandDirectory + $CustomImagesFolder
-}
-
-if(![string]::IsNullOrEmpty($CustomExcelFilesFolder))
-{
-	$DefaultExcelConfigurationFolder = $CommandDirectory + $CustomExcelFilesFolder
-}
-
-if(![string]::IsNullOrEmpty($CustomPropertyTemplateFile))
-{
-	$DefaultPropertyTemplateFile = $CommandDirectory + $CustomPropertyTemplateFile
-	$DefaultPropertyTemplateName = $CustomPropertyTemplateName
-}
-
-$CurrentExcelConfigurationFolder = $DefaultExcelConfigurationFolder
-
-# If the solution is multilingual then the source folder corresponds to the default variation label folder
-if ($IsMultilingual)
-{
-	$CurrentExcelConfigurationFolder = $DefaultExcelConfigurationFolder + "/" + $SourceLabel
-}
-
-# -----------------------------------------------------------------------
-# SHAREGATE SETTINGS
-# -----------------------------------------------------------------------
-
-# Create Copy Settings
-$copySettings = New-CopySettings -OnContentItemExists Overwrite
-
-# -----------------------------------------------------------------------
-# PROCESS IMAGES
-# -----------------------------------------------------------------------
-
-Write-Warning "Importing images..."
-
-# Fix ACLs before importing images
+# Script to fix file ACLs for bug fix with Sharegate import
 $AclScript = $CommandDirectory + "\Restore-AclInheritance.ps1" 
-& $AclScript -folderPath $ImagesConfigurationFolder
 
-$CustomDestinationSite = "[[DSP_PortalAuthoringSiteUrl]]"
-$UploadPicturesInDocCenter = [System.Convert]::ToBoolean("[[DSP_UploadPicturesInDocCenter]]")
 
-if(![string]::IsNullOrEmpty("[[DSP_PortalDocsSiteUrl]]") -and $UploadPicturesInDocCenter)
-{
-	$CustomDestinationSite = "[[DSP_PortalDocsSiteUrl]]"
+# ******************************************
+# Local utility methods
+# ******************************************
+function Get-FullPath {
+    param(
+		[Parameter(Mandatory=$true)]
+        [string]$Path)
+
+    if ([System.IO.Path]::IsPathRooted($Path) -ne $true) {
+        if ($Path.StartsWith(".\")) {
+            $Path = $path.Replace(".\", ($CommandDirectory + "\"))
+        } else {
+            $Path = Join-Path -Path $CommandDirectory -ChildPath $Path
+        }
+    }
+
+    return $Path;
 }
 
-# Add Images to site collection images
-$imagelist = Connect-Site -Url $CustomDestinationSite| Get-List -Name "[[DSP_PictureLibraryName]]"
-Import-Document -DestinationList $imagelist -SourceFolder $ImagesConfigurationFolder | Export-Report -Path ($LogFolderPath + "\Images_ImportReport") -Overwrite
+function Get-CustomPropertyMappings {
 
-# -----------------------------------------------------------------------
-# PROCESS EXCEL FILES
-# -----------------------------------------------------------------------
+    # Custom property mapping settings
+	$MappingSettings = New-MappingSettings 
 
-# Only import data in root authoring webs because of variations syncing
-[[DSP_AuthoringSourceRootWebUrls]] | Foreach-Object { 
+	# Remove default keys
+	Remove-PropertyMapping -MappingSettings $MappingSettings -Destination Title
+	Remove-PropertyMapping -MappingSettings $MappingSettings -Destination Created
 
-	$CurrentAuthoringWeb = $_
+	# Add custom keys
+	Set-PropertyMapping -MappingSettings $MappingSettings -Source DynamiteContentAssociationKey -Destination DynamiteContentAssociationKey -Key
 
-	$Catalogs = [[DSP_CUSTOM_CatalogTitles]]
-	
-	if($Catalogs.Count -eq 0)
-	{
-		$Catalogs = [[DSP_DEFAULT_CatalogTitles]]
-	}
-
-	# Excel Files
-	$Catalogs.Keys | Foreach-Object {
-	
-		$CurrentFileName = $_
-		$CatalogDisplayNames = $Catalogs.Item($_)
-	
-		$CatalogDisplayNames | Foreach-Object {
-
-			$CurrentCatalogName = $_
-
-			# Get a fake random list (not needed in the procedure because we use an Excel file but needed for the cmdlet)
-			$srcList = Connect-Site -Url [[DSP_PortalAuthoringSiteUrl]] | Get-List | Where-Object {$_.BaseType -eq "List"} | Select -First 1
-
-			$dstList = Connect-Site -Url $CurrentAuthoringWeb | Get-List -Name $CurrentCatalogName
-
-			if($dstList -ne $null)
-			{
-				# Trick to get the exact mappings settings for the list
-				$mappingSettings = Get-PropertyMapping -SourceList $dstList -DestinationList $dstList 
-				# Add custom key (internal unique ID)
-				$mappingSettings = Set-PropertyMapping -MappingSettings $mappingSettings -Source DynamiteInternalId -Destination DynamiteInternalId -Key
-				# Remove previous key
-				$mappingSettings = Set-PropertyMapping -MappingSettings $mappingSettings -Source Created -Destination Created
-				$mappingSettings = Set-PropertyMapping -MappingSettings $mappingSettings -Source Title -Destination Title
-
-				# Get the specified file according to the default folder
-				$file = Get-ChildItem -Path $CurrentExcelConfigurationFolder | Where-Object {$_.Name -eq $CurrentFileName}
-				if ($file -ne $null)
-				{		
-					Write-Warning "Importing data into catalog $CurrentCatalogName in web $CurrentAuthoringWeb..."
-					Copy-Content -SourceList $srcList -DestinationList $dstList -ExcelFilePath $file.FullName -MappingSettings $mappingSettings -CopySettings $copySettings | Export-Report -Path ($LogFolderPath + "\" + $CurrentCatalogName + "_ImportReport") -Overwrite
-				}
-			}
-			else
-			{
-				Write-Warning "Unable to find the catalog with name $CurrentCatalogName in web $CurrentAuthoringWeb...Skipping..."
-			}
-		}
-	}
+    return $MappingSettings
 }
 
-if($IsMultilingual)
-{
+function Wait-VariationSyncTimerJob {
+
 	# We don't need to sync manually items because we force the "Approved" status which automatically fires variations event receiver 
 	# Sync item with timer job
 	$WebApplication = Get-SPWebApplication "[[DSP_PortalWebAppUrl]]"
 	Wait-SPTimerJob -Name "VariationsPropagateListItem" -WebApplication $WebApplication
 	Write-Warning "Waiting for 'VariationsPropagateListItem' timer job to finish..."
 	Start-Sleep -Seconds 15
-
-	# Excel Files
-	$Catalogs.Keys | Foreach-Object {
-	
-		$CurrentFileName = $_
-		$CatalogDisplayNames = $Catalogs.Item($_)
-
-		# Update items on target webs
-		$TargetUrls = [[DSP_AuthoringUrlsByLabels]]
-
-		$TargetUrls.Keys | Foreach-Object { 
-	
-			$CurrentAuthoringWeb = $_
-			$Currentlabel =$TargetUrls.Item($_)
-
-			if ($Currentlabel -ne $SourceLabel)
-			{
-				# Get the configuration folder according to the label of the current site
-				$CurrentExcelConfigurationFolder = $DefaultExcelConfigurationFolder + "/" + $Currentlabel
-
-				$CatalogDisplayNames | Foreach-Object {
-
-					$CurrentCatalogName = $_
-
-					$dstList = Connect-Site -Url $CurrentAuthoringWeb | Get-List -Name $CurrentCatalogName
-
-					if($dstList -ne $null)
-					{
-						# Trick to get the exact mappings settings for the list
-						$mappingSettings = Get-PropertyMapping -SourceList $dstList -DestinationList $dstList 
-						# Add custom key (internal unique ID)
-						$mappingSettings = Set-PropertyMapping -MappingSettings $mappingSettings -Source DynamiteInternalId -Destination DynamiteInternalId -Key
-						# Remove previous key (add the title)
-						$mappingSettings = Set-PropertyMapping -MappingSettings $mappingSettings -Source Created -Destination Created
-						$mappingSettings = Set-PropertyMapping -MappingSettings $mappingSettings -Source Title -Destination Title
-
-						# Import Custom Template for the "Content Association Key" 
-						Import-PropertyTemplate -Path $DefaultPropertyTemplateFile -List $dstList
-
-						# Get the specified file according to the default folder
-						$file = Get-ChildItem -Path $CurrentExcelConfigurationFolder | Where-Object {$_.Name -eq $CurrentFileName}
-						if ($file -ne $null)
-						{		
-							Write-Warning "Importing data into catalog $CurrentCatalogName in web $CurrentAuthoringWeb..."
-							Copy-Content -SourceList $srcList -DestinationList $dstList -TemplateName $DefaultPropertyTemplateName -ExcelFilePath $file.FullName -MappingSettings $mappingSettings -CopySettings $copySettings | Export-Report -Path ($LogFolderPath + "\" + $CurrentCatalogName + "_" +  $Currentlabel + "_ImportReport") -Overwrite
-						}
-					}
-					else
-					{
-						Write-Warning "Unable to find the catalog with name $CurrentCatalogName in web $CurrentAuthoringWeb...Skipping..."
-					}
-				}
-			}
-		}
-	}
 }
 
 
+# ******************************************
+# Configure folder to URL mappings
+# ******************************************
+$IsMultilingual = [System.Convert]::ToBoolean("[[DSP_IsMultilingual]]")
+$SourceLabel ="[[DSP_SourceLabel]]"
+
+# If not already defined, create default folder to URL mappings
+if($DSP_MigrationFolderMappings -eq $null) {
+    $DSP_MigrationFolderMappings = [Ordered]@{ ".\Default\Default (english only)" = "[[DSP_PortalAuthoringSiteUrl]]" }
+
+    # If the solution is multilingual then the source folder corresponds to the default variation label folder
+    if ($IsMultilingual) {
+	    $DSP_MigrationFolderMappings = [Ordered]@{ 
+        ".\Default\Multilingual\Root" = "[[DSP_PortalAuthoringSiteUrl]]";
+        ".\Default\Multilingual\Source" = "[[DSP_PortalAuthoringSiteUrl]]/[[DSP_SourceLabel]]" }
+
+        [[DSP_VariationsTargetLabels]] |  Foreach-Object {
+            if ($_ -ne "[[DSP_SourceLabel]]") {
+                $FromFolder = (".\Default\Multilingual\Targets\" + $_)
+                $ToUrl = ("[[DSP_PortalAuthoringSiteUrl]]/" + $_)
+                
+                $DSP_MigrationFolderMappings.Add($FromFolder, $ToUrl)
+            }
+        }
+    }
+}
+
+# ******************************************
+# Configure migration data before import
+# ******************************************
+if ($DSP_MigrationDataConfigurationScript -eq $null) {
+    $DSP_MigrationDataConfigurationScript = ".\Default\Configure-MigrationData.ps1"
+}
+    
+$DSP_MigrationFolderMappings.Keys | ForEach-Object {
+    $Folder = Get-FullPath -Path $_
+    $ConfigurationScript = Get-FullPath -Path $DSP_MigrationDataConfigurationScript
+    & $ConfigurationScript -FolderPath $Folder
+}
+
+# ******************************************
+# Import "non-variation" data
+# ******************************************
+# Defines mappings keys for "non-variation" content and variation targets
+$mappingKeys = $DSP_MigrationFolderMappings.Keys | where { -not $_.ToUpperInvariant().Contains("TARGETS") }
+$mappingKeys | ForEach-Object {
+    $fromFolder = Get-FullPath -Path $_
+    $toUrl = $DSP_MigrationFolderMappings[$_]
+
+    # Fix ACLs before importing data
+    & $AclScript -folderPath $fromFolder
+
+    Import-DSPData -FromFolder $fromFolder -ToUrl $toUrl -LogFolder $LogFolderPath
+}
+
+# ******************************************
+# If multilingual, sync and the process target label data
+# ******************************************
+if($IsMultilingual) {
+
+    # Wait for variations to sync data to target label(s)
+    Wait-VariationSyncTimerJob
+
+    # Get Sharegate custom property mappings
+    $MappingSettings = Get-CustomPropertyMappings
+
+    # Defines mappings keys for variation targets
+    $TargetMappingKeys = $DSP_MigrationFolderMappings.Keys | where { $_.ToUpperInvariant().Contains("TARGETS") }
+    $TargetMappingKeys | ForEach-Object {
+        $FromFolder = Get-FullPath -Path $_
+        $ToUrl = $DSP_MigrationFolderMappings[$_]
+
+        # Fix ACLs before importing data
+        & $AclScript -folderPath $FromFolder
+
+        Import-DSPData -FromFolder $FromFolder -ToUrl $ToUrl -LogFolder $LogFolderPath -MappingSettings $MappingSettings
+    }
+}
