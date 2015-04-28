168da574d594c2575f5992d84b69db5f74617529
Franck Cornu <franck.cornu@gsoft-group.com>
2015-04-28 17:54:52 -0400

    [StandardCMSPublishing] Validate PUB02

--- Source/GSoft.Dynamite.Models.StandardPublishingCMS/Modules/Publishing/PUB_02/Install-PUB02.ps1
+++ Source/GSoft.Dynamite.Models.StandardPublishingCMS/Modules/Publishing/PUB_02/Install-PUB02.ps1
@@ -1,33 +1,12 @@
 ﻿# ----------------------------------------
 # PUB 02: VIEW ITEM DETAILS
 # ----------------------------------------
-
 param([string]$LogFolderPath)
 
-# ********** LOG INIT ********** #
+$UserStory = "PUB_02"
 
 $0 = $myInvocation.MyCommand.Definition
 $CommandDirectory = [System.IO.Path]::GetDirectoryName($0)
-
-if ([string]::IsNullOrEmpty($LogFolderPath))
-{
-	$LogFolderPath = $CommandDirectory
-}
-
-$ScriptName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
-
-$LogTime = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
-$LogFile = $LogFolderPath + "\" + $ScriptName +"_Dynamite_"+$LogTime +".log"
-
-# Stat log transcript
-Start-Transcript -Path $LogFile 
-
-# ***************************** #
-
-# Verbose preference
-$VerbosePreference ="Continue"
-
-$UserStory = "PUB_02"
 
 $values = @{"User Story: " = $UserStory}
 New-HeaderDrawing -Values $Values
@@ -41,8 +20,3 @@ New-HeaderDrawing -Values $Values
 
 $Script = $CommandDirectory + '\Setup-PageLayouts.ps1'
 & $Script -Force
-
-# ********** LOG END ********** #
-# Stop log transcript
-Stop-Transcript
-# ***************************** #
