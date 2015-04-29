b8be0db01c1bc3b1832d25ff100e080695c55d91
Franck Cornu <franck.cornu@gsoft-group.com>
2015-04-29 16:37:12 -0400

    [StandardPublishingCMS] Add VanillaStandardCMS test project

---
+++ Libraries/GSoft.Dynamite.StandardPublishingCMS.2.2.1-pre10440/tools/Modules/Publishing/PUB_02/Install-PUB02.ps1
@@ -0,0 +1,22 @@
+﻿# ----------------------------------------
+# PUB 02: VIEW ITEM DETAILS
+# ----------------------------------------
+param([string]$LogFolderPath)
+
+$UserStory = "PUB_02"
+
+$0 = $myInvocation.MyCommand.Definition
+$CommandDirectory = [System.IO.Path]::GetDirectoryName($0)
+
+$values = @{"User Story: " = $UserStory}
+New-HeaderDrawing -Values $Values
+
+# ==================================== #
+# =========  PAGE LAYOUTS   ========== #
+# ==================================== #
+
+$values = @{"Step: " = "#1 Create Page Layouts"}
+New-HeaderDrawing -Values $Values
+
+$Script = $CommandDirectory + '\Setup-PageLayouts.ps1'
+& $Script -Force
--- Source/GSoft.Dynamite.VanillaStandardCMS.Scripts/Properties/AssemblyInfo.cs
+++ Source/GSoft.Dynamite.VanillaStandardCMS.Scripts/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GSoft.Dynamite.VanillaCrossSiteCMS.Scripts")]
+[assembly: AssemblyTitle("GSoft.Dynamite.VanillaStandardCMS.Scripts")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GSoft.Dynamite.VanillaCrossSiteCMS.Scripts")]
+[assembly: AssemblyProduct("GSoft.Dynamite.VanillaStandardCMS.Scripts")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b8571dbc-4fa7-4e22-80ab-301fb742afce")]
+[assembly: Guid("66d6c154-bebb-4685-af5c-1fea4c274460")]
 
 // Version information for an assembly consists of the following four values:
 //
