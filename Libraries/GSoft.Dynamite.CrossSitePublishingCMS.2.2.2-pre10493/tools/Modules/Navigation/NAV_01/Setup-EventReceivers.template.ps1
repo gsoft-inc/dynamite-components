73f39961b4f3b95ef01502b037aadb96d41e5820
Franck Cornu <franck.cornu@gsoft-group.com>
2015-05-11 14:14:33 -0400

    [VanillaCrossSiteCMS] Update nuget package to pre10459

---
+++ Libraries/GSoft.Dynamite.CrossSitePublishingCMS.2.2.2-pre10493/tools/Modules/Navigation/NAV_01/Setup-EventReceivers.template.ps1
@@ -0,0 +1,15 @@
+﻿# -----------------------------------------------------------------------
+# Copyright		: GSoft @2014
+# Model  		: Cross Site Publishing CMS
+# File          : Setup-EventReceivers.ps1.template
+# Description	: Attach event receivers
+# -----------------------------------------------------------------------
+
+Write-Warning "Applying Event Receivers configuration..."
+
+# Activate feature on the root web on the authoring site collection
+Initialize-DSPFeature -Url [[DSP_PortalAuthoringSiteUrl]]  -Id [[DSP_CrossSitePublishingCMS_NAV_BrowsableItemsEventReceivers]]
+
+# Common CMS hook to add nav-related event receivers to any CT you like on the publishing site 
+# (by default the INavigationEventReceivers config is empty)
+Initialize-DSPFeature -Url [[DSP_PortalPublishingSiteUrl]]  -Id [[DSP_CommonCMS_NAV_EventReceivers]]
