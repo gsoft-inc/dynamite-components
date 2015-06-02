755cf39ef2b7d9bdb5a944119ab59ab07d8715af
Philippe Lavoie <philippe.lavoie@gsoft.com>
2015-06-02 13:06:24 -0400

    [Coding Style] Replace tabs by spaces.

--- Source/GSoft.Dynamite.Models.StandardPublishingCMS/Install-Model.template.ps1
+++ Source/GSoft.Dynamite.Models.StandardPublishingCMS/Install-Model.template.ps1
@@ -1,9 +1,9 @@
 ﻿<#
 .SYNOPSIS
-	Installs the cross-site publishing model.
-	
+    Installs the cross-site publishing model.
+    
 .DESCRIPTION
-	Installs solution packages and modules for this model.
+    Installs solution packages and modules for this model.
 #>
 
 # ********** PRE-FLIGHT CHECK ********** #
@@ -15,17 +15,17 @@ $ErrorActionPreference = "Stop"
 
 # Solutions deployment check
 if (-not (Test-DSPDeployedSolutions -SolutionsConfigurationFilePath "$(Get-Location)\Solutions\Default\Default-Solutions.xml")) { 
-	Write-Error -Message "Solutions defined in '.\Solutions\Default\Default-Solutions.xml' are not correctly deployed."
+    Write-Error -Message "Solutions defined in '.\Solutions\Default\Default-Solutions.xml' are not correctly deployed."
 }
 if (Test-Path -Path "$(Get-Location)\Solutions\Custom\Custom-Solutions.xml") {
-	if (-not (Test-DSPDeployedSolutions -SolutionsConfigurationFilePath "$(Get-Location)\Solutions\Custom\Custom-Solutions.xml")) { 
-		Write-Error -Message "Solutions defined in '.\Solutions\Default\Custom-Solutions.xml' are not correctly deployed."
-	}
+    if (-not (Test-DSPDeployedSolutions -SolutionsConfigurationFilePath "$(Get-Location)\Solutions\Custom\Custom-Solutions.xml")) { 
+        Write-Error -Message "Solutions defined in '.\Solutions\Default\Custom-Solutions.xml' are not correctly deployed."
+    }
 }
 
 # Check if Sharegate is installed
 if ((Get-Module | where { $_.Name -eq "Sharegate" }).Count -ne 1) {
-	Write-Error -Message "Sharegate PowerShell module is not correctly installed."
+    Write-Error -Message "Sharegate PowerShell module is not correctly installed."
 }
 
 # TODO: Nice to have a check on which service locator is loaded into the GAC
@@ -36,42 +36,42 @@ $header = @{"Solution Model: " = "[StandardPublishingCMS]";}
 New-HeaderDrawing -Values $header
 
 try {
-	#region ********** PUBLISHING MODULE ********** #
-	.\Modules\Publishing\PUB_01\Install-PUB01.ps1
-	.\Modules\Publishing\PUB_02\Install-PUB02.ps1
-	#endregion
+    #region ********** PUBLISHING MODULE ********** #
+    .\Modules\Publishing\PUB_01\Install-PUB01.ps1
+    .\Modules\Publishing\PUB_02\Install-PUB02.ps1
+    #endregion
 
-	#region ********** NAVIGATION MODULE ********** #
-	.\Modules\Navigation\NAV_01\Install-NAV01.ps1
-	.\Modules\Navigation\NAV_05\Install-NAV05.ps1
-	#endregion
+    #region ********** NAVIGATION MODULE ********** #
+    .\Modules\Navigation\NAV_01\Install-NAV01.ps1
+    .\Modules\Navigation\NAV_05\Install-NAV05.ps1
+    #endregion
 
-	#region ********** DESIGN MODULE ********** #
-	.\Modules\Design\DSGN_01\Install-DSGN01.ps1
-	#endregion
+    #region ********** DESIGN MODULE ********** #
+    .\Modules\Design\DSGN_01\Install-DSGN01.ps1
+    #endregion
 
-	#region ********** MULTILINGUALISM MODULE ********** #
-	# Check Multilingual settings
-	$IsMultilingual = [System.Convert]::ToBoolean("[[DSP_IsMultilingual]]")
-	if($IsMultilingual)
-	{
-		# Why LANG02 before LANG01?
-		.\Modules\Multilingualism\LANG_02\Install-LANG02.ps1
-		.\Modules\Multilingualism\LANG_01\Install-LANG01.ps1
-		#.\Modules\Multilingualism\LANG_03\Install-LANG03.ps1
-	}
-	#endregion
+    #region ********** MULTILINGUALISM MODULE ********** #
+    # Check Multilingual settings
+    $IsMultilingual = [System.Convert]::ToBoolean("[[DSP_IsMultilingual]]")
+    if($IsMultilingual)
+    {
+        # Why LANG02 before LANG01?
+        .\Modules\Multilingualism\LANG_02\Install-LANG02.ps1
+        .\Modules\Multilingualism\LANG_01\Install-LANG01.ps1
+        #.\Modules\Multilingualism\LANG_03\Install-LANG03.ps1
+    }
+    #endregion
 
-	#region ********** POST DEPLOYMENT SCRIPTS ********** #
-	.\Execute-PostDeploymentScript.ps1
-	#endregion
+    #region ********** POST DEPLOYMENT SCRIPTS ********** #
+    .\Execute-PostDeploymentScript.ps1
+    #endregion
 } catch {
     # Output error for logging to catch it
-	$_ | Out-String
-	throw
+    $_ | Out-String
+    throw
 } finally {
-	# ********** STOP LOGGING ********** #
-	Stop-DSPLogging
+    # ********** STOP LOGGING ********** #
+    Stop-DSPLogging
 }
 
 
