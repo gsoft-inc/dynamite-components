12755e4b3e34863b30cbd363ba3c7096b8af7225
Alexandre Massé <alexandre.masse@gsoft-group.com>
2015-06-12 13:59:56 -0400

    [Modules] Added the Targeting module.

---
+++ Source/GSoft.Dynamite.Targeting.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GSoft.Dynamite.Targeting.Core")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2abf2f1b-bbbf-4b85-a351-76f93f031864")]
