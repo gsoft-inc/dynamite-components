67ad2301c707f989094d4b72d04767a601199e03
Alexandre Massé <alexandre.masse@gsoft-group.com>
2015-06-15 12:40:29 -0400

    [Targeting] - Added features activation to the scripts of the targeting module.

--- Source/GSoft.Dynamite.Models.CrossSitePublishingCMS/Modules/Targeting/TARGET_01/Setup-EventReceivers.template.ps1
+++ Source/GSoft.Dynamite.Models.CrossSitePublishingCMS/Modules/Targeting/TARGET_01/Setup-EventReceivers.template.ps1
@@ -1,12 +1,11 @@
 ﻿# -----------------------------------------------------------------------
 # Copyright		: GSoft @2015
 # Model  		: Cross Site Publishing CMS
-# File          : Setup-Fields.ps1.template
-# Description	: Create fields
+# File          : Setup-EventReceivers.ps1.template
+# Description	: Attach event receivers
 # -----------------------------------------------------------------------
 
-Write-Warning "Applying Fields configuration..."
+Write-Warning "Applying Event Receivers configuration..."
 
 # Activate feature on the root web on the authoring site collection
-Initialize-DSPFeature -Url [[DSP_PortalAuthoringSiteUrl]]  -Id [[DSP_CrossSitePublishingCMS_TARGET_Fields]]
-
+Initialize-DSPFeature -Url [[DSP_PortalAuthoringSiteUrl]]  -Id [DSP_CrossSitePublishingCMS_TARGET_EventReceivers]]
