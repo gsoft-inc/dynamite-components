98cbc6617270e7dc505bc0d4c91e749ed0fd0842
Yohan Belval <yohan.belval@gsoft.com>
2015-06-15 11:38:39 -0400

    [Targeting] Added the script files for the Targeting module.  Still needs feature IDs for field, content types, result sources and event receivers.

---
+++ Source/GSoft.Dynamite.Models.CrossSitePublishingCMS/Modules/Targeting/TARGET_01/Install-TARGET01.ps1
@@ -0,0 +1,29 @@
+﻿# ----------------------------------------
+# TARGET 01: CREATE TARGETABLE CONTENT
+# ----------------------------------------
+
+param([string]$LogFolderPath)
+
+$UserStory = "TARGET_01"
+
+$0 = $myInvocation.MyCommand.Definition
+$CommandDirectory = [System.IO.Path]::GetDirectoryName($0)
+
+$values = @{"User Story: " = $UserStory}
+New-HeaderDrawing -Values $Values
+
+# =========================================== #
+# =========   CATEGORIZE CONTENTS =========== #
+# =========================================== #
+
+$values = @{"Step: " = "#1 Setup Columns"}
+New-HeaderDrawing -Values $Values
+
+$Script = $CommandDirectory + '\Setup-Fields.ps1'
+& $Script
+
+$values = @{"Step: " = "#2 Setup Content Types"}
+New-HeaderDrawing -Values $Values
+
+$Script = $CommandDirectory + '\Setup-ContentTypes.ps1'
+& $Script 
---
+++ Source/GSoft.Dynamite.Models.CrossSitePublishingCMS/Modules/Targeting/TARGET_01/Setup-Fields.template.ps1
@@ -0,0 +1,12 @@
+﻿# -----------------------------------------------------------------------
+# Copyright		: GSoft @2015
+# Model  		: Cross Site Publishing CMS
+# File          : Setup-Fields.ps1.template
+# Description	: Create fields
+# -----------------------------------------------------------------------
+
+Write-Warning "Applying Fields configuration..."
+
+# Activate feature on the root web on the authoring site collection
+Initialize-DSPFeature -Url [[DSP_PortalAuthoringSiteUrl]]  -Id [[DSP_CommonCMS_TARGET_Fields]]
+
