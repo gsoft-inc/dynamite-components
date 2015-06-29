b662a6eea930741e5f5da2d106d41dc9e34cabdb
Yohan Belval <yohan.belval@gsoft.com>
2015-06-29 07:25:54 -0400

    [Social] Added social module projects and nuget package entry.

---
+++ Source/GSoft.Dynamite.Social.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GSoft.Dynamite.Social.Core")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("38b8e1ed-f8eb-4c44-9d30-76a08c512eab")]
