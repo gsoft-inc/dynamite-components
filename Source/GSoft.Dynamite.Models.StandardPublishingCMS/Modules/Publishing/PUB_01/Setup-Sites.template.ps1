bf2d1b4e08798777518a8f22e862cc18d5c844aa
Thomas-Antoine O'Neill <thomasantoine.oneill@gsoft.com>
2015-07-02 15:31:32 -0400

    [PUB_01] Move the Setup-Sites logic that initializes the property bag and the top-level variation webs outside of the -Force switch if-else. - This way, even if we don't -Force a re-creation of the site collection, the property bag and the variations will be initialized.

--- Source/GSoft.Dynamite.Models.StandardPublishingCMS/Modules/Publishing/PUB_01/Setup-Sites.template.ps1
+++ Source/GSoft.Dynamite.Models.StandardPublishingCMS/Modules/Publishing/PUB_01/Setup-Sites.template.ps1
@@ -31,34 +31,34 @@ if ($Force) {
 
 	# Create the new SharePoint structure
 	New-DSPStructure $ConfigurationFilePath
-	
-	# FIRST THING TO DO ONCE SITE COLLECTIONS ARE CREATED:
-	# Configure Service Locator assembly name preference (if left un-initialized, the default FallbackServiceLocator will be used - 
-	# or, if found, the first DLL matching *ServiceLocator.DLL in the GAC will be used)
-	$assemblyName = "[[DSP_ServiceLocatorAssemblyName]]"
-	if ([string]::IsNullOrEmpty($assemblyName) -eq $false)
-	{
-		# assume that the same service locator should be used on authoring and publishing site collections
-		Set-DSPWebProperty -Url "[[DSP_PortalPublishingSiteUrl]]" -Key "ServiceLocatorAssemblyName" -Value $assemblyName
-		Set-DSPWebProperty -Url "[[DSP_PortalAuthoringSiteUrl]]" -Key "ServiceLocatorAssemblyName" -Value $assemblyName
-		Set-DSPWebProperty -Url "[[DSP_PortalDocsSiteUrl]]" -Key "ServiceLocatorAssemblyName" -Value $assemblyName
-	}
-
-	# Check Multilingual settings
-	$IsMultilingual = [System.Convert]::ToBoolean("[[DSP_IsMultilingual]]")
-
-	if($IsMultilingual)
-	{
-		$values = @{"Step: " = "#1.1 Setup Sites Variations"}
-		New-HeaderDrawing -Values $Values
-
-		Write-Warning "Applying Site Variations configuration..."
-
-		# Activate feature on the root web on the publishing site collection
-		Initialize-DSPFeature -Url [[DSP_PortalPublishingHostNamePath]]  -Id [[DSP_CommonCMS_LANG_CreateVariationsHierarchies]]
-	}
 }
 else
 {
 	Write-Warning "'Force' parameter was not specified. Skipping creation..."
+}
+
+
+# FIRST THING TO DO ONCE SITE COLLECTIONS ARE CREATED:
+# Configure Service Locator assembly name preference (if left un-initialized, the default FallbackServiceLocator will be used - 
+# or, if found, the first DLL matching *ServiceLocator.DLL in the GAC will be used)
+$assemblyName = "[[DSP_ServiceLocatorAssemblyName]]"
+if ([string]::IsNullOrEmpty($assemblyName) -eq $false)
+{
+	# assume that the same service locator should be used on authoring and publishing site collections
+	Set-DSPWebProperty -Url "[[DSP_PortalPublishingSiteUrl]]" -Key "ServiceLocatorAssemblyName" -Value $assemblyName
+	Set-DSPWebProperty -Url "[[DSP_PortalAuthoringSiteUrl]]" -Key "ServiceLocatorAssemblyName" -Value $assemblyName
+	Set-DSPWebProperty -Url "[[DSP_PortalDocsSiteUrl]]" -Key "ServiceLocatorAssemblyName" -Value $assemblyName
+}
+
+# Check Multilingual settings
+$IsMultilingual = [System.Convert]::ToBoolean("[[DSP_IsMultilingual]]")
+
+if($IsMultilingual) {
+	$values = @{"Step: " = "#1.1 Setup Sites Variations"}
+	New-HeaderDrawing -Values $Values
+
+	Write-Warning "Applying Site Variations configuration..."
+
+	# Activate feature on the root web on the publishing site collection
+	Initialize-DSPFeature -Url [[DSP_PortalPublishingHostNamePath]] -Id [[DSP_CommonCMS_LANG_CreateVariationsHierarchies]]
 }
