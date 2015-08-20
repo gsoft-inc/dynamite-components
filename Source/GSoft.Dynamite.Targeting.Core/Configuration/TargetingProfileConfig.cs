32346ebb0eaa5a07d7233593b933075ba9333bcd
Alexandre Massé <alexandre.masse@gsoft-group.com>
2015-08-20 10:52:12 -0400

    [Targeting] Needed to be able to modify the TargetingProfileTaxonomySyncJob.The custom job is now registered in Autofac as a SPJobDefinition, and the feature is resolving it. You can now register your own custom job, register it, and the feature will use it. To make it work, the TimerJobName is now specified in the TargetingProfileConfig. Also, added a SyncTaxonomyFields method in the SyncService, which checks all the user profiles with empty target fields, and sync them.

--- Source/GSoft.Dynamite.Targeting.Core/Configuration/TargetingProfileConfig.cs
+++ Source/GSoft.Dynamite.Targeting.Core/Configuration/TargetingProfileConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using GSoft.Dynamite.Targeting.Contracts.Configuration;
+using GSoft.Dynamite.Targeting.Contracts.Constants;
 using GSoft.Dynamite.UserProfile;
 using Microsoft.SharePoint;
 
@@ -12,6 +13,20 @@ namespace GSoft.Dynamite.Targeting.Core.Configuration
     /// </summary>
     public class TargetingProfileConfig : ITargetingProfileConfig
     {
+        /// <summary>
+        /// Gets the name of the timer job.
+        /// </summary>
+        /// <value>
+        /// The name of the timer job.
+        /// </value>
+        public string TimerJobName
+        {
+            get
+            {
+                return TargetingConstants.TargetingTimerJobName;
+            }
+        }
+
         /// <summary>
         /// Gets the timer job schedule.
         /// </summary>
---
+++ Source/GSoft.Dynamite.Targeting.SP/RegistrationModules/TargetingSPRegistrationModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Autofac;
+using GSoft.Dynamite.Targeting.SP.TimerJobs;
+using Microsoft.SharePoint.Administration;
+
+namespace GSoft.Dynamite.Targeting.SP.RegistrationModules
+{
+    /// <summary>
+    /// Portal Targeting Registration Module for the dependencies injection engine
+    /// </summary>
+    public class TargetingSPRegistrationModule : Module
+    {
+        /// <summary>
+        /// Registers the modules type bindings
+        /// </summary>
+        /// <param name="builder">
+        /// The builder.
+        /// </param>
+        protected override void Load(ContainerBuilder builder)
+        {
+            // Timer Jobs
+            builder.RegisterType<TargetingProfileTaxonomySyncJob>().As<SPJobDefinition>();
+        }
+    }
+}
